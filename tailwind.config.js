159b6f559a97decd6407455968ac87498abe6964
aulakh-savreet <147961994+aulakh-savreet@users.noreply.github.com>
2024-12-10 01:23:59 -0700

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#0D3B66',    // Deep Blue for trust and adventure
-        secondary: '#FAF3DD',  // Warm Light Cream for balance
-        accent: '#F4D35E',     // Bright Yellow for energy and excitement
-        highlight: '#EE964B',  // Warm Orange for exploration and vibrance
-        neutral: '#F2F4F3',    // Soft Gray-White for clean backgrounds
-        // Removed 'dark' color as dark mode is disabled
-        success: '#7CB518',    // Vibrant Green for positivity
-        warning: '#C72C41',    // Deep Red for alerts or warnings
+        primary: '#0D3B66',    
+        secondary: '#FAF3DD',  
+        accent: '#F4D35E',     
+        highlight: '#EE964B',  
+        neutral: '#F2F4F3',    
+        success: '#7CB518',    
+        warning: '#C72C41',    
       },
       fontFamily: {
-        sans: ['Poppins', 'sans-serif'], // Modern and clean font
-        serif: ['Roboto Slab', 'serif'], // Classic and bold
+        sans: ['Poppins', 'sans-serif'], 
+        serif: ['Roboto Slab', 'serif'], 
       },
     },
   },
